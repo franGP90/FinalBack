3ef09a532ad700bb2edb6823e3003f5363ccdb8f
franGP90 <kiko.gp2003@gmail.com>
2025-12-17 23:02:00 +0100

    bd iniciada

---
+++ src/db/mongo.ts
@@ -0,0 +1,27 @@
+import { Db, MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let client: MongoClient;
+let dB: Db;
+const dbName = "ejemploFinal";
+
+export const connectToMongoDB = async () => {
+    try{
+        const mongoUrl = process.env.MONGO_URL;
+        if(mongoUrl){
+            client = new MongoClient(mongoUrl);
+            await client.connect();
+            dB = client.db(dbName);
+            console.log("Conexión a mongo exitosa");
+        } else {
+            throw new Error("MONGO_URL is not defined in environment variables");
+        }
+    }
+    catch(err){
+        console.log("Error al conectarse a mongo: ", err)
+    }
+};
+
+export const getDB = ():Db => dB;
